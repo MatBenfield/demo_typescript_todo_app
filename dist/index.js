80cc10ae36d7af6ed026f89da8116e159c42f55a
MatBenfield <MatBenfield@users.noreply.github.com>
2020-10-23 15:00:17 +0100

    first commit of to do app
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

---
+++ dist/index.js
@@ -0,0 +1,97 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const inquirer = __importStar(require("inquirer"));
+const jsonTodoCollection_1 = require("./jsonTodoCollection");
+let todos = [
+    new todoItem_1.TodoItem(1, "Buy Flowers"), new todoItem_1.TodoItem(2, "Get Shoes"),
+    new todoItem_1.TodoItem(3, "Collect Tickets"), new todoItem_1.TodoItem(4, "Call Joe", true)
+];
+let collection = new jsonTodoCollection_1.JsonTodoCollection("Adam", todos);
+let showCompleted = true;
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Add"] = "Add New Task";
+    Commands["Complete"] = "Complete Task";
+    Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Purge"] = "Remove Completed Tasks";
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptAdd() {
+    console.clear();
+    inquirer.prompt({ type: "input", name: "add", message: "Enter task:" })
+        .then(answers => {
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
+        }
+        promptUser();
+    });
+}
+function promptComplete() {
+    console.clear();
+    inquirer.prompt({ type: "checkbox", name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map(item => ({ name: item.task, value: item.id, checked: item.complete }))
+    }).then(answers => {
+        let completedTasks = answers["complete"];
+        collection.getTodoItems(true).forEach(item => collection.markComplete(item.id, completedTasks.find(id => id === item.id) != undefined));
+        promptUser();
+    });
+}
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands),
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                }
+                else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
+        }
+    });
+}
+promptUser();
